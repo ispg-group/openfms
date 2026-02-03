47309b5c55f28bdff144594a26fbd16b70831d24
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2026-02-03 12:22:22 +0000

    Pass shellcheck

--- run_fms_tc.sh
+++ run_fms_tc.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# Must have intel, intel MPI, TeraChem, and FMS90 in environment
+# Must have MPI, TeraChem, and FMS90 in environment
 # Must pass GPUS, TCEXE, and FMSEXE in on commandline as env variables
 # Example: GPUS=0,1 TCEXE=terachem FMSEXE=FMS.tc.e ./run.sh
-echo "GPUS: $GPUS"
-echo "TCEXE: $TCEXE"
-echo "FMSEXE: $FMSEXE"
-echo "MPI_ADDITIONAL_ARGUMENT: $MPI_ADDITIONAL_ARGUMENT (must be -nameserver localhost for intel MPI)"
+TCEXE=terachem
+FMSEXE=openfms.tc
+GPUS=0
+
+MPI_ADDITIONAL_ARGUMENT="-nameserver localhost"
 
 # Generate random port number to avoid conflicts
-server=tcfms_port$[ ( $RANDOM % 10000 ) + 1]
-printf "&tc\nserver_name = '$server'\n" > tc_input
+server=tcfms_port$(( ( RANDOM % 10000 ) + 1 ))
+printf "&tc\nserver_name = '%s'\n/\n" "$server" > tc_input
 
-if [ -z $(ps aux | grep hydra_nameserver | grep -v "grep" | awk '{print $2}') ]; then hydra_nameserver & sleep 1; fi
+if pgrep hydra_nameserve; then hydra_nameserver & sleep 1; fi
+
 # For MPICH, OpenFabrics interface works in general
 export MPIR_CVAR_CH4_NETMOD=ofi
+
 # mpiexec.hydra seems to be the most effective way to start the two processes
-mpiexec.hydra $MPI_ADDITIONAL_ARGUMENT -n 1 $TCEXE -g $GPUS -U2 --MPIPort=$server > tc.out &
+mpiexec.hydra "$MPI_ADDITIONAL_ARGUMENT" -n 1 "$TCEXE" -g "$GPUS" -U2 --MPIPort="$server" &> tc.out &
 PID1=$!
-sleep 5 # grace time for terachem initialization (FMS mode initialization doesn't include GPU startup, so it should be fast)
-mpiexec.hydra $MPI_ADDITIONAL_ARGUMENT -n 1 $FMSEXE > fms.out &
+sleep 5 # grace time for terachem initialization
+
+mpiexec.hydra "$MPI_ADDITIONAL_ARGUMENT" -n 1 "$FMSEXE" &> fms.out &
 PID2=$!
 
 # Should be replace with "wait -n" once we have bash 4.3
