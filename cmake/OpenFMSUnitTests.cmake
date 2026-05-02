34d4a78bcf48cbeecae9896408d58cf7b646ddf5
Eirik F. Kjønstad <eirik.kjonstad@ntnu.no>
2026-05-01 18:52:58 -0700

    Adjust to review comments

--- cmake/OpenFMSUnitTests.cmake
+++ cmake/OpenFMSUnitTests.cmake
@@ -1,24 +1,11 @@
 function(openfms_add_unit_tests openfms_core_target)
-  # OpenFMS unit tests are registered inside the Fortran sources. At configure
-  # time, a small Python scanner converts those registrations into CTest calls
-  # in the generated file OpenFMSDiscoveredUnitTests.cmake
-  find_package(Python3 REQUIRED COMPONENTS Interpreter)
-
-  # Build one Fortran test runner from every unit_tests/*.F90 file. The
-  # generated CTest entries below invoke this runner with a suite and test name.
-  file(GLOB OPENFMS_UNIT_TEST_SOURCES
-    CONFIGURE_DEPENDS
-    "${CMAKE_CURRENT_SOURCE_DIR}/unit_tests/*.F90"
-  )
-  file(GLOB OPENFMS_UNIT_TEST_SUITE_SOURCES
-    CONFIGURE_DEPENDS
-    "${CMAKE_CURRENT_SOURCE_DIR}/unit_tests/test_*.F90"
-  )
-  # Reconfigure when unit tests are added, removed, or re-registered 
-  set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS
-    "${CMAKE_CURRENT_SOURCE_DIR}/unit_tests/main.F90"
-    ${OPENFMS_UNIT_TEST_SUITE_SOURCES}
-    "${CMAKE_CURRENT_SOURCE_DIR}/cmake/discover_openfms_unit_tests.py"
+  set(OPENFMS_UNIT_TEST_SOURCES
+    unit_tests/main.F90
+    unit_tests/test_bundle.F90
+    unit_tests/test_particle.F90
+    unit_tests/test_trajectory.F90
+    unit_tests/testdrive.F90
+    unit_tests/testutils.F90
   )
 
   add_executable(openfms_unit_tests ${OPENFMS_UNIT_TEST_SOURCES})
@@ -35,37 +22,23 @@ function(openfms_add_unit_tests openfms_core_target)
   )
   openfms_configure_fortran_target(openfms_unit_tests)
 
-  set(_openfms_discovered_unit_tests
-    "${CMAKE_CURRENT_BINARY_DIR}/OpenFMSDiscoveredUnitTests.cmake"
+  set(OPENFMS_UNIT_TEST_SUITES
+    BundleModule
+    ParticleModule
+    TrajectoryModule
   )
-  
-  execute_process(
-    COMMAND
-      "${Python3_EXECUTABLE}"
-      "${CMAKE_CURRENT_SOURCE_DIR}/cmake/discover_openfms_unit_tests.py"
-    WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
-    RESULT_VARIABLE _openfms_unit_test_discovery_result
-  )
-  # Error-code ≠ 0 => something went wrong with python script
-  if(NOT _openfms_unit_test_discovery_result EQUAL 0)
-    message(FATAL_ERROR "OpenFMS unit test discovery failed")
-  endif()
-  include("${_openfms_discovered_unit_tests}")
-  
-endfunction()
 
-function(openfms_add_discovered_unit_test suite_name test_name)
-  # Called from the generated CMake include (OpenFMSDiscoveredUnitTests.cmake). 
-  # Each CTest entry selects a single Fortran unit test by passing the suite/test pair
-  set(_openfms_ctest_name "unit.${suite_name}.${test_name}")
+  foreach(suite_name IN LISTS OPENFMS_UNIT_TEST_SUITES)
+    set(_openfms_ctest_name "unit.${suite_name}")
 
-  add_test(
-    NAME "${_openfms_ctest_name}"
-    COMMAND "$<TARGET_FILE:openfms_unit_tests>" "${suite_name}" "${test_name}"
-    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-  )
-  set_tests_properties("${_openfms_ctest_name}" PROPERTIES
-    LABELS "unit;${suite_name}"
-  )
+    add_test(
+      NAME "${_openfms_ctest_name}"
+      COMMAND "$<TARGET_FILE:openfms_unit_tests>" "${suite_name}"
+      WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    )
+    set_tests_properties("${_openfms_ctest_name}" PROPERTIES
+      LABELS "unit;${suite_name}"
+    )
+  endforeach()
 
 endfunction()
--- unit_tests/main.F90
+++ unit_tests/main.F90
@@ -1,6 +1,6 @@
 program tester
    use, intrinsic :: iso_fortran_env, only: output_unit, error_unit
-   use testdrive, only: run_testsuite, run_selected, new_testsuite, testsuite_type, &
+   use testdrive, only: run_testsuite, new_testsuite, testsuite_type, &
                         init_color_output, get_final_message
    use testutils, only: unit_test_error_handler, check_dieerror_not_called
    use test_particle, only: collect_particle_suite
@@ -11,19 +11,16 @@ program tester
    integer :: num_failed_tests, is, num_args, selected_suite_index
    type(testsuite_type), allocatable :: testsuites(:)
    character(len=*), parameter :: fmt = '("#", *(1x, a))'
-   character(len=255) :: force_color, selected_suite, selected_test
+   character(len=255) :: force_color, selected_suite
 
    num_failed_tests = 0
-   selected_suite = ''
-   selected_test = ''
 
    num_args = command_argument_count()
-   if (num_args > 2) then
-      write (error_unit, '(a)') 'Usage: openfms_unit_tests [suite [test]]'
+   if (num_args > 1) then
+      write (error_unit, '(a)') 'Usage: openfms_unit_tests [suite]'
       stop 1
    end if
-   if (num_args >= 1) call get_command_argument(1, selected_suite)
-   if (num_args >= 2) call get_command_argument(2, selected_test)
+   if (num_args == 1) call get_command_argument(1, selected_suite)
 
    ! Decide whether the output should be colourful:
    ! 1. Respect the FORCE_COLOR envvar per https://force-color.org/
@@ -49,17 +46,17 @@ program tester
    ! error handler defined in testutils.F90.
    call set_error_handler(unit_test_error_handler)
 
-   if (num_args >= 1) then
+   if (num_args == 1) then
       selected_suite_index = find_suite(testsuites, selected_suite)
       if (selected_suite_index == 0) then
          call print_available_suites(testsuites, selected_suite)
          stop 1
       end if
 
-      call run_suite(testsuites(selected_suite_index), selected_test, num_args >= 2, num_failed_tests)
+      call run_suite(testsuites(selected_suite_index), num_failed_tests)
    else
       do is = 1, size(testsuites)
-         call run_suite(testsuites(is), selected_test, .false., num_failed_tests)
+         call run_suite(testsuites(is), num_failed_tests)
       end do
    end if
 
@@ -98,19 +95,12 @@ contains
       end do
    end subroutine print_available_suites
 
-   subroutine run_suite(testsuite, selected_test, only_selected_test, num_failed_tests)
+   subroutine run_suite(testsuite, num_failed_tests)
       type(testsuite_type), intent(in) :: testsuite
-      character(len=*), intent(in) :: selected_test
-      logical, intent(in) :: only_selected_test
       integer, intent(inout) :: num_failed_tests
 
       write (error_unit, fmt) "Testing:", testsuite%name
-      if (only_selected_test) then
-         call run_selected(testsuite%collect, trim(selected_test), error_unit, num_failed_tests)
-         if (num_failed_tests < 0) stop 1
-      else
-         call run_testsuite(testsuite%collect, error_unit, num_failed_tests)
-      end if
+      call run_testsuite(testsuite%collect, error_unit, num_failed_tests)
 
       ! Make sure FMS_DieError was not called unexpectedly.
       call check_dieerror_not_called()
