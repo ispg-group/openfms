0d9e607be0af372bfca21862a9e78b1d02c16d54
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2026-07-07 13:10:50 +0100

    Try fixing CMDLINE test

--- tests/CMDLINE/test.sh
+++ tests/CMDLINE/test.sh
@@ -10,16 +10,16 @@ fi
 FMSEXE=$1
 
 # Test that ABIN fails with invalid cmdline argument
-$FMSEXE -invalid > fms.out 2>> err.out
+$FMSEXE -invalid > fms.out 2> err.out
 
 # Test that ABIN prints help, without an error
-$FMSEXE -h >> fms.out 2>> err.out || echo "ERROR when printing help" >> error.out
-$FMSEXE --help >> fms.out 2>> err.out || echo "ERROR when printing help" >> error.out
+$FMSEXE -h >> fms.out 2>> err.out
+$FMSEXE --help >> fms.out 2>> err.out
 
 # Test that ABIN prints version, without an error
-$FMSEXE -v >> fms.out 2>> err.out || echo "ERROR when printing version" >> error.out
-$FMSEXE --version >> fms.out 2>> err.out || echo "ERROR when printing version" >> error.out
+$FMSEXE -v >> fms.out 2>> err.out
+$FMSEXE --version >> fms.out 2>> err.out
 
 # Filter out "STOP X" or "X" from stderr, where X is the return code
 # This hack is tested for gfortran and intel compilers
-grep -E -v '^(STOP )?[01]$' err.out > errors.dat
+grep -E -v '^(STOP)?[[:space:]]*[01]$' err.out >> errors.dat
